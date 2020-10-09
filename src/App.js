95ce5cd1c8b4a59fa91f46c8501de5107ff64d17
derkweijers <derk@weijers.xyz>
2020-10-09 09:05:40 +0200

    Using Roboto fonts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, ActivityIndicator } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import {
+  useFonts,
+  Roboto_900Black,
+  Roboto_100Thin,
+  Roboto_400Regular,
+} from '@expo-google-fonts/roboto';
 
 import { LaunchText } from './components';
 import { launchesToday } from './services';
 
 export default function App() {
   const [launches, setLaunches] = useState(null);
+  let [fontsLoaded] = useFonts({
+    Roboto_900Black,
+    Roboto_100Thin,
+    Roboto_400Regular,
+  });
 
   useEffect(() => {
     (async () => {
@@ -14,7 +25,7 @@ export default function App() {
     })();
   }, []);
 
-  if (launches === null) {
+  if (!fontsLoaded || launches === null) {
     return (
       <View style={styles.container}>
         <ActivityIndicator color="black" size="large" />
