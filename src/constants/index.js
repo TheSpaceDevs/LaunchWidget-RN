1fbf0f15945741c7598274e0fa2db2087ea1a54f
derkweijers <derk@weijers.xyz>
2020-10-09 09:06:03 +0200

    Load the dev url when running in dev mode

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,9 @@
-export const LL_URL = 'https://ll.thespacedevs.com';
+let LL_URL;
+
+if (process.env.NODE_ENV === 'development') {
+  LL_URL = 'https://lldev.thespacedevs.com';
+} else {
+  LL_URL = 'https://ll.thespacedevs.com';
+}
+
+export { LL_URL };
