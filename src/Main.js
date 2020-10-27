96c4c58f8b24e282bac154cf0af309752f055673
derkweijers <derk@weijers.xyz>
2020-10-27 14:41:36 +0100

    Added Sentry, because that isn't overkill at all for this app

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import * as Sentry from '@sentry/react-native';
 
 import App from './App';
 import AppContext from './AppContext';
+
+Sentry.init({
+  dsn:
+    'https://f9569860f6d34873bee6d5222f4c665e@o167118.ingest.sentry.io/5494832',
+});
 
 const Main = () => {
   return (
