639c0cd4f5186abf612e0b15fb86783f6ae23e09
derkweijers <derk@weijers.xyz>
2020-10-27 14:04:10 +0100

    Using .env to set token during build

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Config from 'react-native-config';
 
 import { LL_URL } from '../constants';
 
@@ -6,10 +7,20 @@ const getUpcomingLaunches = async () => {
   try {
     const response = await axios.get(`${LL_URL}/2.0.0/launch/upcoming`, {
       timeout: 5000,
+      headers: {
+        Authorization: `Token ${Config.LL_TOKEN}`,
+        'User-Agent': 'Is There A Launch Today?',
+      },
     });
     return response.data.results;
   } catch (e) {
-    alert('Error contacting the LL2 API');
+    if (e.response.status === 429) {
+      alert('Too many requests from this IP');
+    } else {
+      alert('Error contacting the LL2 API');
+    }
+
+    return [];
   }
 };
 
